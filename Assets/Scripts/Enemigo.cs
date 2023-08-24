7eabb1d04f2d836af8a69fe729b1dc655bfa4fa3
flopox <l.llop@alumno.etec.um.edu.ar>
2023-08-24 12:28:01 -0300

    seguimiento de enemigo

--- Assets/Scripts/Enemigo.cs
+++ Assets/Scripts/Enemigo.cs
@@ -2,11 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Enemigo : MonoBehaviour
 {
+	private PlayerScript player;
+	[SerializeField]
+	float step;
+	private Vector3 target;
+	
+	void Start(){
+		player = FindObjectOfType < PlayerScript >();
+		target = player.GetComponent < Transform >().position;
+	}
+
+	void Update(){
+		 transform.position = Vector2.MoveTowards(transform.position, target, step);
+	}
     private void OnCollisionEnter2D(Collision2D other) {
 		if(other.gameObject.CompareTag("Player")) {
 			GameManager.Instance.PerderVidas();
 		}
 	}
+
 }
